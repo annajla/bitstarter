e660b6dfe04b870e307ca5d6c69eda91e91cc702
Nagla A <annajla@gmail.com>
2013-08-11 20:23:27 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(fs.readFile('index.html'),String);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
